da71e18eb28ff675d0553470e024b71a3643afca
Fife Oluwabunmi <oluwabunmifife@gmail.com>
2023-06-21 14:30:50 +0100

    Setting up details view

--- questions/urls.py
+++ questions/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import question_list
+from .views import question_list, question_details
 
 urlpatterns = [
-    path('question/', question_list, name='qlist')
+    path('question/', question_list, name='qlist'),
+    path('question/<slug:slug>', question_details, name='qdetails') 
 ]
--- questions/views.py
+++ questions/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 
 # Create your views here.
 def question_list(request):
     question_lister = Question.objects.all().order_by('-created_at')
     return render(request, 'QList.html', {"question_lister": question_lister})
+
+def question_details(request, slug):
+    question = get_object_or_404(Question, slug=slug)
+    return render(request, 'QDetails.html', {'question': question})
