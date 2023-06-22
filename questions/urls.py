0b606586fcb3e49141f307b1c899d920527fcb8f
Fife Oluwabunmi <oluwabunmifife@gmail.com>
2023-06-22 20:40:55 +0100

    Registration system implemented

--- questions/urls.py
+++ questions/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import question_list, question_details
+from .views import question_list, question_details, register
 
 urlpatterns = [
     path('question/', question_list, name='qlist'),
-    path('question/<slug:slug>', question_details, name='qdetails') 
+    path('question/<slug:slug>', question_details, name='qdetails'),
+    path('register/', register, name='register'),
 ]
--- questions/views.py
+++ questions/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Question
+from .forms import UserRegistrationForm
 
 # Create your views here.
 def question_list(request):
@@ -9,3 +10,17 @@ def question_list(request):
 def question_details(request, slug):
     question = get_object_or_404(Question, slug=slug)
     return render(request, 'QDetails.html', {'question': question})
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data["password"])
+
+            new_user.save()
+            return render(request, "register_done.html", {"user_form": user_form})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, "register.html", {"user_form": user_form})
