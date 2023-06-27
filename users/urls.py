4afc95c3127e9b3f79ebc8c105ae506b8f274827
Fife Oluwabunmi <oluwabunmifife@gmail.com>
2023-06-27 11:20:55 +0100

    Users can change passwords

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout') 
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('change_password/', PasswordChangeView.as_view(), name = 'change_password'),
+    path('change_password/done/', PasswordChangeDoneView.as_view(), name = 'password_change_done'),
 ]
